ce7850515b1f8a456a7d23146def37539709e703
Taku-pg <kaitsu505@gmail.com>
2025-12-10 15:56:49 +0100

    create database schema

---
+++ Backend/src/models/db.js
@@ -0,0 +1,23 @@
+const sqlite3 = require('sqlite3').verbose();
+const fs = require('fs');
+const path = require('path');
+
+const dbPath=path.join(__dirname,'../../db/database.db');
+const sqlPath=path.join(__dirname,'../../db/init.sql');
+
+console.log(dbPath);
+
+const db = new sqlite3.Database(
+    dbPath, 
+    sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, 
+    (err) => {
+    if (err) console.error('connection error: ' + err.message);
+});
+
+const init = fs.readFileSync(sqlPath, 'utf-8');
+
+db.exec(init, (err) => {
+    if (err) console.error('execution error: ' + err.message);
+});
+
+module.exports = db; 
